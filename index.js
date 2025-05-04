787a51baa926c438c733ed3b3fc760f09ec03e43
KirillArturov <curillaenator@gmail.com>
2025-05-04 19:01:57 +0700

    fix: upds

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 require("dotenv").config();
 
-const https = require("https");
-const fs = require("fs");
+const firebaseAdmin = require("firebase-admin");
+const firebaseServiceAccount = require("./firebase-service-account.json");
+
+// const https = require("https");
+// const fs = require("fs");
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const WEB_APP_URL = process.env.WEB_APP_URL;
+const WEB_APP_ORIGIN = process.env.WEB_APP_ORIGIN;
+const TG_BOTNAME = process.env.TG_BOTNAME;
 
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(firebaseServiceAccount),
+  databaseURL:
+    "https://art-app-2020-default-rtdb.europe-west1.firebasedatabase.app",
+});
+
+const fsdb = firebaseAdmin.firestore();
 const app = express();
 
-app.use(cors({ origin: WEB_APP_URL, methods: ["GET", "POST"] }));
+// app.use(cors({ origin: WEB_APP_ORIGIN, methods: ["GET", "POST"] }));
+app.use(cors());
+
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -21,12 +34,40 @@ const tgBot = new TelegramBot(BOT_TOKEN, { polling: true });
 tgBot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  tgBot.sendMessage(chatId, "Please click Dive to continue 👽", {
+  tgBot.sendMessage(chatId, "Please click Start App", {
     reply_markup: {
-      keyboard: [[{ text: "Dive", web_app: { url: WEB_APP_URL } }]],
+      keyboard: [
+        [
+          {
+            text: "Start App",
+            web_app: {
+              url: `${WEB_APP_ORIGIN}?botname=${TG_BOTNAME}&stamp=${chatId}`,
+            },
+          },
+        ],
+      ],
       resize_keyboard: true,
     },
   });
+
+  try {
+    const chatId = msg.chat.id;
+
+    const payload = {
+      msgId: msg.message_id,
+      chat: msg.chat,
+      from: msg.from,
+      date: msg.date,
+    };
+
+    fsdb
+      .collection(`${TG_BOTNAME}chats`)
+      .doc(`${chatId}`)
+      .set(payload, { merge: true })
+      .then(() => payload);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.get("/", (req, res) => {
@@ -34,33 +75,44 @@ app.get("/", (req, res) => {
 });
 
 app.post("/bot-data", async (req, res) => {
-  const { queryId, content, clientSupport } = req.body;
+  console.log("BOT_DATA", req.body);
 
-  if (!queryId || !content || !clientSupport) {
-    return res.status(400).json({ error: "Missing required fields" });
+  const { actionType, payload } = req.body;
+
+  switch (actionType) {
+    case "remind":
+      const { chats = [], message } = payload;
+
+      chats.forEach((chatId) => tgBot.sendMessage(chatId, message));
+
+      tgBot.sendMessage();
+      break;
+
+    default:
+      break;
   }
 
-  try {
-    tgBot.answerWebAppQuery(queryId, {
-      id: queryId,
-      type: "article",
-      title: "Success!",
-      input_message_content: { message_text: `${content}\n\n${clientSupport}` },
-    });
+  // try {
+  //   tgBot.answerWebAppQuery(queryId, {
+  //     id: queryId,
+  //     type: "article",
+  //     title: "Success!",
+  //     input_message_content: { message_text: `${content}\n\n${clientSupport}` },
+  //   });
 
-    return res.status(200).json({});
-  } catch (error) {
-    console.error("AnswerWebAppQuery error:", error);
+  //   return res.status(200).json({});
+  // } catch (error) {
+  //   console.error("AnswerWebAppQuery error:", error);
 
-    tgBot.answerWebAppQuery(queryId, {
-      type: "article",
-      id: queryId,
-      title: "Error!",
-      input_message_content: { message_text: "Something went wrong" },
-    });
+  //   tgBot.answerWebAppQuery(queryId, {
+  //     type: "article",
+  //     id: queryId,
+  //     title: "Error!",
+  //     input_message_content: { message_text: "Something went wrong" },
+  //   });
 
-    return res.status(500).json({});
-  }
+  //   return res.status(500).json({});
+  // }
 });
 
 app.listen(PORT, () => {
